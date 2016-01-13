acc0522bfcff07b8b13c428c2da2f0b5eaa25e2a
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2016-01-13 10:07:25 +0200

    emailing service is ready

--- gmail.go
+++ gmail.go
@@ -1,70 +1,37 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
-
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
+	"net/smtp"
+	"strconv"
 )
 
-// tokenFromFile retrieves a Token from a given file path.
-// It returns the retrieved Token and any read error encountered.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	t := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(t)
-	defer f.Close()
-	return t, err
+type EmailUser struct {
+	Username    string
+	Password    string
+	EmailServer string
+	Port        int
 }
 
-func connectToGmail() *http.Client {
-	ctx := context.Background()
+func sendEmail(emailTo string, emailFrom string, subject string, body string, username string, password string) {
+	to := []string{emailTo}
+	msg := []byte("To: " + emailTo + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"\r\n" +
+		body +
+		"\r\n")
 
-	b, err := ioutil.ReadFile("client_secret.json")
+	emailUser := &EmailUser{username, password, "smtp.gmail.com", 587}
+
+	auth := smtp.PlainAuth("", emailUser.Username, emailUser.Password, emailUser.EmailServer)
+
+	err := smtp.SendMail(emailUser.EmailServer+":"+strconv.Itoa(emailUser.Port),
+		auth,
+		emailUser.Username,
+		to,
+		msg)
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-
-	tok, err := tokenFromFile("client_auth.json")
-	client := config.Client(ctx, tok)
-	return client
-
-}
-
-func getLabels(client *http.Client) {
-
-	srv, err := gmail.New(client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve gmail Client %v", err)
-	}
-
-	user := "me"
-	r, err := srv.Users.Labels.List(user).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve labels. %v", err)
-	}
-	if len(r.Labels) > 0 {
-		fmt.Print("Labels:\n")
-		for _, l := range r.Labels {
-			fmt.Printf("- %s\n", l.Name)
-		}
-	} else {
-		fmt.Print("No labels found.")
+		log.Print("ERROR: attempting to send a mail ", err)
 	}
 
 }
